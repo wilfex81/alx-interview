4b3b87f681e3e47d0643a7f6f3143194fc871778
Wilfex <84175893+wilfex81@users.noreply.github.com>
2023-01-18 21:35:52 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,41 @@
 #!/usr/bin/python3
-""" doc """
+"""Input stats"""
 import sys
 
+stats = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+sizes = [0]
 
-if __name__ == "__main__":
-    i = 0
-    status = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0
-    }
-    fileSize = 0
 
-    def printstats(fileSize, status):
-        """ doc """
-        print("File size: {:d}".format(fileSize))
-        for key in sorted(status.keys()):
-            if status[key] != 0:
-                print("{}: {:d}".format(key, status[key]))
+def print_stats():
+    print('File size: {}'.format(sum(sizes)))
+    for s_code, count in sorted(stats.items()):
+        if count:
+            print('{}: {}'.format(s_code, count))
 
-    try:
-        for line in sys.stdin:
-            words = line.split()
-            if len(words) >= 2:
-                if words[-2] in status.keys():
-                    status[words[-2]] += 1
-                fileSize += int(words[-1])
-                i += 1
-                if not i % 10:
-                    printstats(fileSize, status)
-        printstats(fileSize, status)
-    except KeyboardInterrupt:
-        printstats(fileSize, status)
-        raise
+
+try:
+    for i, line in enumerate(sys.stdin, start=1):
+        matches = line.rstrip().split()
+        try:
+            status_code = matches[-2]
+            file_size = matches[-1]
+            if status_code in stats.keys():
+                stats[status_code] += 1
+            sizes.append(int(file_size))
+        except Exception:
+            pass
+        if i % 10 == 0:
+            print_stats()
+    print_stats()
+except KeyboardInterrupt:
+    print_stats()
+    raise
